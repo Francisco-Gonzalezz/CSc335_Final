00a98a0741f573e4f9166f12c950550391aa4e10
reesethan <ethanrees@arizona.edu>
2022-11-29 09:35:24 -0700

    fixed a tiny thing

--- CSC335_Final/src/_main/Boot.java
+++ CSC335_Final/src/_main/Boot.java
@@ -1,12 +1,6 @@
 package _main;
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
-
 import ui.SceneManager;
-import ui.UIAnimator;
 import ui.titleScreen.TitleScreenUI;
-import ui.wordleGameBoard.WordleGameBoardUI;
 
 public class Boot {
 	public static void main(String[] args) {
