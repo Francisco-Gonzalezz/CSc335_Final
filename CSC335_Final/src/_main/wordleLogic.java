151bffa1bc5ec7a533394b4b938c298f98929a5e
reesethan <ethanrees@arizona.edu>
2022-12-01 09:35:10 -0700

    updated the game logic (from Aditya)

--- CSC335_Final/src/_main/wordleLogic.java
+++ CSC335_Final/src/_main/wordleLogic.java
@@ -1,31 +1,28 @@
 package _main;
+
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class wordleLogic
+public class wordleLogic 
 {
-	public static void main(String[] args) throws IOException
+	static String correctWord;
+	static String filename = "src/words.txt";
+	
+	public static void fileReader(String filename) throws IOException
 	{
-		final String bgGreen = "\u001B[42m";
-		final String BgYellow = "\u001B[43m";
-		final String reset = "\u001B[0m";
-
-		System.out.println("Let's play WORDLE!");
-		
 		List<String> listOfString = new ArrayList<String>();
-		
-		BufferedReader bf = new BufferedReader(new FileReader("src/words.txt"));
+
+		BufferedReader bf = new BufferedReader(new FileReader(filename));
 
 		String line = bf.readLine();
 
 		while (line != null)
 		{
-			listOfString.add(line);
+			listOfString.add(line.toUpperCase());
 			line = bf.readLine();
 		}
 
@@ -33,81 +30,62 @@ public class wordleLogic
 		bf.close();
 
 		// storing the data in arraylist to array
-		String[] words
-		= listOfString.toArray(new String[0]);
-
-
-		//List of words!
-		//Read from textfile
-		//Input exception, check if word exists in game - TODO
-
-		//String[] words = {"DERBY", "FLANK", "GHOST", "WINCH", "JUMPS"};
-
-		//picking a random word
+		String[] words = listOfString.toArray(new String[0]);
 		int wordIndex = (int)(Math.random() * words.length);
-		String correct = words[wordIndex];
-
-		Scanner sc = new Scanner(System.in);
-		String guess = ""; 
-
-		//Input exception, add only characters
-		//Input exception, cut when exceeds more than 5 characters
-		//Input exception, add more words to complete 5 characters
-
-		System.out.print("Guess the word!\n");
-
-		//loop for guesses
-		for(int round = 0; round < 6; round++)
+		correctWord = words[wordIndex];
+		
+		
+		System.out.println(correctWord);
+	}
+	
+	public static void main(String[] args) throws IOException
+	{
+		fileReader(filename);
+		
+		for(int i = 0; i < 5; i++)
 		{
-			//System.out.println();
-			guess = sc.nextLine().toUpperCase();
-
-			if(checkWord(guess) && check_for_word(guess))
-			{
-				//loop iterating through each letter of word
-				for(int i = 0; i < 5; i++)
-				{
-					if(guess.substring(i, i + 1).equals(correct.substring(i, i + 1)))
-					{
-						//characters match
-						System.out.print(bgGreen + guess.substring(i, i + 1) + reset);
-					}
-					else if(correct.indexOf(guess.substring(i, i + 1)) > -1)
-					{
-						//character match but not location
-						System.out.print(BgYellow + guess.substring(i, i + 1) + reset);
-					}
-					else
-					{
-						//letter doesn't exist
-						System.out.print(guess.substring(i, i + 1));
-					}
-				}
-
-				System.out.println("");
-			}
-			else
-			{
-				round--;
-				System.out.println("Invalid String");
-			}
-			
-			//if guess is correct
-			if(guess.equals(correct))
-			{
-				System.out.println("Win Win!");
-				break;
-			}
-
-		}
-
-		//print if guess is wrong
-		if(!guess.equals(correct))
-		{
-			System.out.println("Wrong! Correct word: " + correct);
+			System.out.println(i);
+			getTheWord(correctWord);
 		}
 	}
+	
+	public static void getTheWord(String correctWord)
+	{
+		final String bgGreen = "\u001B[42m";
+		final String BgYellow = "\u001B[43m";
+		final String reset = "\u001B[0m";
 
+		Scanner sc = new Scanner(System.in);
+		String guess = sc.nextLine().toUpperCase();
+		
+		if(checkWord(guess) && check_for_word(guess))
+		{
+			//loop iterating through each letter of word
+			for(int i = 0; i < 5; i++)
+			{
+				if(guess.substring(i, i + 1).equals(correctWord.substring(i, i + 1)))
+				{
+					//characters match
+					System.out.print(bgGreen + guess.substring(i, i + 1) + reset);
+				}
+				else if(correctWord.indexOf(guess.substring(i, i + 1)) > -1)
+				{
+					//character match but not location
+					System.out.print(BgYellow + guess.substring(i, i + 1) + reset);
+				}
+				else
+				{
+					//letter doesn't exist
+					System.out.print(guess.substring(i, i + 1));
+				}
+			}
+
+			System.out.println("");
+		}
+
+
+	}
+	
 	public static boolean checkWord(String word) 
 	{
 		return word.length()>=5 && word.matches("[a-zA-Z]+");
@@ -117,7 +95,7 @@ public class wordleLogic
     {
         try 
         {
-            BufferedReader in = new BufferedReader(new FileReader("src/words.txt"));
+            BufferedReader in = new BufferedReader(new FileReader(filename));
             String str;
             while ((str = in.readLine()) != null) 
             {
@@ -127,10 +105,11 @@ public class wordleLogic
                 }
             }
             in.close();
-        } catch (IOException e) 
+        } catch (IOException e)
         {
         	
         }
         return false;
     }
+	
 }
